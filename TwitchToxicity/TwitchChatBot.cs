6dafd69c4829745711f50c4e3445ff2b59e9ec29
nogna <nogna@NOGNA>
2018-09-10 15:05:00 +0200

    fuck it

--- TwitchToxicity/TwitchChatBot.cs
+++ TwitchToxicity/TwitchChatBot.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using TwitchLib.Client;
 using TwitchLib.Client.Events;
 using TwitchLib.Client.Models;
@@ -9,7 +11,10 @@ namespace TwitchToxicity
     class TwitchChatBot
     {
         TwitchClient client;
-        string CHANNELNAME = "Nognaz";
+        string CHANNELNAME = "dakotaz";
+        List<double> TOXICITY = new List<double>();
+        int LIST_LENGTH = 100;
+        
 
         SentimentIntensityAnalyzer analyzer = new SentimentIntensityAnalyzer();
 
@@ -25,28 +30,45 @@ namespace TwitchToxicity
             client.OnMessageReceived += onMessageReceived;
             client.OnJoinedChannel += onJoinedChannel;
             client.Connect();
-
         }
 
         private void onJoinedChannel(object sender, OnJoinedChannelArgs e)
         {
-            Console.WriteLine("Hey guys! I am a bot connected via TwitchLib!");
-            client.SendMessage(e.Channel, "Hey guys! I am a bot connected via TwitchLib!");
+            Console.WriteLine("Hey guys! I am a bot connected via TwitchLib!, connected in channel: "+ e.Channel);
+            //client.SendMessage(e.Channel, "Hey guys! I am a bot connected via TwitchLib! " + "Channel name: "+ e.Channel);
         }
 
         private void onMessageReceived(object sender, OnMessageReceivedArgs e)
         {
-            Console.WriteLine("Someone wrote this message: " + e.ChatMessage.Message);
+            // Console.WriteLine("Someone wrote this message: " + e.ChatMessage.Message);
             string text = e.ChatMessage.Message;
-
             var results = analyzer.PolarityScores(text);
 
-            Console.WriteLine("Positive score: " + results.Positive);
-            Console.WriteLine("Negative score: " + results.Negative);
-            Console.WriteLine("Neutral score: " + results.Neutral);
-            Console.WriteLine("Compound score: " + results.Compound);
+            if (text == "!toxicity" && TOXICITY.Count() > 0)
+            {
+                Console.WriteLine("The current level of toxicity is: "+ TOXICITY.Average());
+                client.SendMessage(e.ChatMessage.Channel, "The current level of toxicity is: " + TOXICITY.Average());
+                return;
+            }
 
+            if (results.Compound == 0)
+            {
+                return;
+            }
 
+            var listLength = TOXICITY.Count; 
+            if (listLength < LIST_LENGTH)
+            {
+                TOXICITY.Add(results.Compound);
+                
+            }
+            else
+            {
+                TOXICITY.Remove(0);
+                TOXICITY.Add(results.Compound);
+            }
+
+            
 
         }
 
